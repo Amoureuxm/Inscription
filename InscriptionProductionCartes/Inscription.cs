6cb9f1c0fcd540ecd797f41f3c9fd8df33a1ee6b
Phil Bauma <evlukoobauma@gmail.com>
2024-05-02 17:32:49 +0200

    CodeDocumentation

--- InscriptionProductionCartes/Inscription.cs
+++ InscriptionProductionCartes/Inscription.cs
@@ -17,11 +17,17 @@ namespace InscriptionProductionCartes
 {
     public partial class Inscription : Form
     {
+        //=========================================================================
         //INSTANCIATION DE LA VARIABLE TEMP DU PROFIL
 
         private Image Profil;
 
-        public  DataForm GetDataForm()
+        //=========================================================================
+
+            //STORE DATA IN PRIVATE VARIRABLES
+
+        //=========================================================================
+        public DataForm GetDataForm()
         {
             return new DataForm
             {
@@ -33,21 +39,31 @@ namespace InscriptionProductionCartes
                 promotion = promotionBox.Text,
                 numEtudiant = numEtudiantBox.Text,
                 imageEtudiant = Profil
-                //etudiantPicture.Image = new Bitmap(image.FileName)
+                
 
         };
-            
-            
+
+            //=========================================================================
+
+
         }
-        
+
         public Inscription()
         {
             InitializeComponent();
+
+            //FOR GRADIENT BACKGROUND
+
+            //=========================================================================
             panel1.Paint += new PaintEventHandler(panel1_Paint);
+
+            //=========================================================================
         }
 
 
         //EDITING ROWS
+
+        //=========================================================================
 
         void EditRows()
         {
@@ -107,8 +123,9 @@ namespace InscriptionProductionCartes
             }
         }
 
+        //=========================================================================
 
-        //BACKGROUND SETTINGS
+        //GRADIENT BACKGROUND SETTINGS
 
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -127,10 +144,13 @@ namespace InscriptionProductionCartes
             }
         }
 
-        //Mettre placeholder
+        /*
+        //=========================================================================
+
+        //PUT ET REMOVE PLACEHOLDER IN TEXTBOX
         void Placeholder(TextBox box, string word)
         {
-            if (string.IsNullOrWhiteSpace(box.Text)/*!box.Equals("")*/)
+            if (string.IsNullOrWhiteSpace(box.Text)/*!box.Equals(""))
             {
                 box.Text = word;
                 box.ForeColor = Color.Gray;
@@ -159,6 +179,10 @@ namespace InscriptionProductionCartes
                 ForeColor = Color.Black;
             }
         }
+
+
+        //=========================================================================
+        */
 
         //Table Grideview
 
@@ -414,162 +438,12 @@ namespace InscriptionProductionCartes
             Place(emailBox);
         }
 
-        /*
-        //DOCUMENTATION
-
-            private void button1_Click(object sender, EventArgs e)
-        {
-
-            //// Capturer le contenu du formulaire en tant qu'image
-            //Bitmap bmp = new Bitmap(this.Width, this.Height);
-            //this.DrawToBitmap(bmp, new Rectangle(0, 0, this.Width, this.Height));
-
-            //// Préparer l'impression
-            //PrintDocument printDoc = new PrintDocument();
-            ////printDoc.PrintController += ;
-            //printDoc.PrintPage += (s, eArgs) =>
-            //{
-            //    // Calculer les dimensions pour imprimer l'image en entier
-            //    Rectangle pageArea = eArgs.PageBounds;
-            //    float ratio = Math.Min((float)pageArea.Width / bmp.Width, (float)pageArea.Height / bmp.Height);
-            //    int printWidth = (int)(bmp.Width * ratio);
-            //    int printHeight = (int)(bmp.Height * ratio);
-            //    int marginLeft = (pageArea.Width - printWidth) / 2;
-            //    int marginTop = (pageArea.Height - printHeight) / 2;
-
-            //    // Dessiner l'image sur le document d'impression
-            //    eArgs.Graphics.DrawImage(bmp, marginLeft, marginTop, printWidth, printHeight);
-            //    eArgs.HasMorePages = false;
-            //};
-
-            //// Afficher la boîte de dialogue d'impression
-            //PrintDialog printDialog = new PrintDialog();
-            //printDialog.Document = printDoc;
-            //if (printDialog.ShowDialog() == DialogResult.OK)
-            //{
-            //    printDoc.Print();
-            //}
-
-            
-        }
-
-
-
+       
         //Pour les fenetres
         //this.WindowState = FormWindowState.Minimized;
         //this.Close();
         //this.WindowState = FormWindowState.Maximized;
 
-
-        //CODE JAVA POUR EDIT DATAGRIDVIEW
-
-        /*private void tableVenteMouseClicked(java.awt.event.MouseEvent evt) {
-            // TODO add your handling code here:
-            DefaultTableModel table = (DefaultTableModel)tableVente.getModel();
-            String tableId = table.getValueAt(tableVente.getSelectedRow(), 0).toString();
-            String tableName = table.getValueAt(tableVente.getSelectedRow(), 1).toString();
-            String tableCode = table.getValueAt(tableVente.getSelectedRow(), 2).toString();
-            String tableProduit = table.getValueAt(tableVente.getSelectedRow(), 3).toString();
-            String tableQuantite = table.getValueAt(tableVente.getSelectedRow(), 4).toString();
-            String tablePrixUnit = table.getValueAt(tableVente.getSelectedRow(), 5).toString();
-            String tablePrixTotal = table.getValueAt(tableVente.getSelectedRow(), 6).toString();
-
-
-            idBox.setText(tableId);
-            nomClientBox.setText(tableName);
-            codeBox.setText(tableCode);
-            quantiteBox.setText(tableQuantite);
-            prixUnitBox.setText(tablePrixUnit);
-            prixTotalBox.setText(tablePrixTotal);
-
-
-        }
-
-        private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {
-            // TODO add your handling code here:
-            DefaultTableModel table = (DefaultTableModel)tableVente.getModel();
-            if (tableVente.getSelectedRowCount() == 1)
-            {
-                String id = idBox.getText();
-                String nom = nomClientBox.getText();
-                String code = codeBox.getText();
-                String produit = produitBox.getSelectedItem().toString();
-                String quantite = quantiteBox.getText();
-                String prixUnit = prixUnitBox.getText();
-                String prixTotal = prixUnitBox.getText();
-
-
-                table.setValueAt(id, tableVente.getSelectedRow(), 0);
-                table.setValueAt(nom, tableVente.getSelectedRow(), 1);
-                table.setValueAt(code, tableVente.getSelectedRow(), 2);
-                table.setValueAt(produit, tableVente.getSelectedRow(), 3);
-                table.setValueAt(quantite, tableVente.getSelectedRow(), 4);
-                table.setValueAt(prixUnit, tableVente.getSelectedRow(), 5);
-                table.setValueAt(prixTotal, tableVente.getSelectedRow(), 6);
-                JOptionPane.showMessageDialog(this, "Update sucessfully");
-
-            }
-            else if (tableVente.getRowCount() == 0)
-            {
-                JOptionPane.showMessageDialog(this, "Le tableau est vide");
-            }
-            else { JOptionPane.showMessageDialog(this, "Please select single to update"); }
-
-
-        }
-        }
-        
-            //CODE C# POUR EDIT DATAGRIDVIEW
-            
-            foreach (DataGridViewRow row in FicheInscription.Rows)
-            {
-                try
-                {
-                    if (row.IsNewRow)
-                    {
-                        string matricule = FicheInscription.Rows[0].Cells["Matricule"].Value.ToString();
-                        string nom = row.Cells["Nom"].Value.ToString();
-                        string postnom = row.Cells["Postnom"].Value.ToString();
-                        string prenom = row.Cells["Prenom"].Value.ToString();
-                        string genre = row.Cells["Genre"].Value.ToString();
-                        string date = row.Cells[5].Value.ToString();
-                        string lieuNaissance = row.Cells[6].Value.ToString();
-                        string etatCivil = row.Cells["Etat civil"].Value.ToString();
-                        string Addresse = row.Cells["Addresse"].Value.ToString();
-                        string phone = row.Cells["N° Tel"].Value.ToString();
-                        string nomPere = row.Cells["Nom du père"].Value.ToString();
-                        string nomMere = row.Cells["Nom de la mère"].Value.ToString();
-                        string pourcentage = row.Cells["Pourcentage"].Value.ToString();
-                        string province = row.Cells["Province"].Value.ToString();
-                        string territoire = row.Cells["Territoire"].Value.ToString();
-                        string dossier = row.Cells["Dossier déposé"].Value.ToString();
-
-
-
-                        matriculeBox.Text = matricule;
-                        nomBox.Text = nom;
-                        postNomBox.Text = postnom;
-                        prenomBox.Text = prenom;
-                        genreBox.Text = genre;
-                        dateBox.Text = date;
-                        lieuBox.Text = lieuNaissance;
-                        etatCivilBox.Text = etatCivil;
-                        addresseBox.Text = Addresse;
-                        telBox.Text = phone;
-                        nomPereBox.Text = nomPere;
-                        nomMereBox.Text = nomMere;
-                        pourcentBox.Text = pourcentage;
-                        provinceBox.Text = province;
-                        territoireBox.Text = territoire;
-                        dossierBox.Text = dossier;
-                    }
-                    
-                } catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-            */
 
         private void FicheInscription_Click(object sender, EventArgs e)
         {
